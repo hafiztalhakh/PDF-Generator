3d245788e239dbd9003ceb6dc8a720fbc26dde45
talha619 <hafiz.talhakh@gmail.com>
2020-04-08 21:09:20 +0500

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
 
     state = {
         name: '',
-        reciptId: '',
+        receiptId: '',
         price1: '',
         price2: '',
     }
@@ -19,7 +19,7 @@ export default class App extends Component {
         return (
             <div className="App">
                 <input type="text" placeholder="Name" name="name" onChange={(e) => { this.setState({ name: e.target.value }) }} />
-                <input type="number" placeholder="Recipt ID" name="reciptId" onChange={(e) => { this.setState({ reciptId: e.target.value }) }} />
+                <input type="number" placeholder="Recipt ID" name="receiptId" onChange={(e) => { this.setState({ receiptId: e.target.value }) }} />
                 <input type="number" placeholder="Price 1" name="price1" onChange={(e) => { this.setState({ price1: e.target.value }) }} />
                 <input type="number" placeholder="Price 2" name="price2" onChange={(e) => { this.setState({ price2: e.target.value }) }} />
                 <button onClick={this.createPdf}>Submit</button>
